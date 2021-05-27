f5aa94467cb6dfdf76d56f353b606a82141619ce
alisalameh1 <>
2021-05-27 13:31:55 +0000

    java

---
+++ app.js
@@ -0,0 +1,21 @@
+var x = 10;
+var y = 5;
+var z = x + y;
+var not = "This is firs prototype "
+console.log(z);
+
+alert(not);
+
+
+var my = prompt("wirte your age ");
+
+
+if (z >= my){
+  document.write("<p> you are young </p>")
+  document.write("<p> your age below 15 </P>")
+  console.log(my);
+}else {
+  document.write("<p> you are adult </P>")
+  document.write("<p> your age above 15 </P>")
+  console.log(my);
+}
